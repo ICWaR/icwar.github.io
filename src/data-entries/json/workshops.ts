05af0b21c442093a8f366d17d96033529e06403e
krispy <krishnapandeykp@users.noreply.github.com>
2024-01-05 18:55:34 +0530

    Data added to Workshop.ts and corresponding images to src.

--- src/data-entries/json/workshops.ts
+++ src/data-entries/json/workshops.ts
@@ -69,5 +69,163 @@ export const workshops=[
     imgsArray:[
       '../../assets/images/Workshop and Meetings/UPSCAPE.jpg'
     ]
-  }
+  },
+  {
+    Date: '23 SEP, 2019',
+    Title:'Towards-smart and Nutrition-sensitive Food Systems in India',
+    Details: 'Meeting held during 23rd to 25th September, 2019 on "Towards-smart and Nutrition-sensitive Food Systems in India"',
+    'links':[
+      ''],
+    imgsArray:[
+      '../../assets/images/Workshop and Meetings/23-25Sept,2019.jpg'
+    ]
+  },
+  {
+    Date: '05 AUG, 2019',
+    Title:'Monsoon School held during 5th-10th August, 2019',
+    Details: 'Interdisciplinary Center for Water Research (ICWaR), Indian Institute of Science (IISc), Bangalore organised a one-week Monsoon School on Urban Floods during 05 – 10 August, 2019. The School was sponsored by the Knowledge Societies Division of UNESCO, Paris and is partially supported by the research project entitled, “Urban Flood Model for Bangalore“, funded by Department of Science & Technology (DST), Government of India.',
+    'links':[
+      ''],
+    imgsArray:[
+      '../../assets/images/Workshop and Meetings/Monsoon-School_Group_photo-2019.jpg'
+    ]
+  },
+  {
+    Date: '02 AUG, 2019',
+    Title:'National Workshop on Urban Climate Network',
+    Details: 'This workshop was funded by DST and was held in ICWaR on 2nd and 3rd August, 2019.',
+    'links':[
+      ''],
+    imgsArray:[
+      '../../assets/images/Workshop and Meetings/2-3Aug-2019.jpg'
+    ]
+  },
+  {
+    Date: '01 AUG, 2019',
+    Title:'Project Steering Meeting for the project “National Supercomputing Mission Urban”',
+    Details: 'Project Steering Meeting for the project “National Supercomputing Mission Urban” held in ICWaR on 1st August, 2019',
+    'links':[
+      ''],
+    imgsArray:[
+      '../../assets/images/Workshop and Meetings/NSM-Aug2019.1.jpg',
+      '../../assets/images/Workshop and Meetings/NSM-Aug2019.2.jpg',
+      '../../assets/images/Workshop and Meetings/NSM-Aug2019.3.jpg'
+    ]
+  },
+  {
+    Date: '23 APR, 2019',
+    Title:'Annual Progress Meet for the project “Urban Floods Model for Bangalore City”',
+    Details: 'The Urban flood Model Team from IISc and KSNDMC visited the "School of Ancient Wisdom" on 23rd April, 2019. A progress meet was being held at the venue where the project partners from BBMP, BDA, Geokno, Spatika, CStep and KSCST were present.',
+    'links':[
+      'https://sites.google.com/site/urbanfloodmodelbangalore/consortium/iisc-bangalore'],
+    imgsArray:[
+      '../../assets/images/Workshop and Meetings/UFMBC-1.jpg',
+      '../../assets/images/Workshop and Meetings/UFMBC-2.jpg',
+      '../../assets/images/Workshop and Meetings/UFMBC-3.jpg',
+      '../../assets/images/Workshop and Meetings/UFMBC-4.jpg'
+    ]
+  },
+  {
+    Date: '12 MAR, 2019',
+    Title:'Project Meet on Upscaling Catchment Processses for Sustainable Water Management in Peninsular India (UPSCAPE)',
+    Details: 'A project consortium meeting for the collaborative UPSCAPE project with CEH, ATREE, BGS, University of Dundee, ICRISAT etc., was conducted on 12th, 13th and 14th March, 2019 in ICWaR, IISc.',
+    'links':[
+      ''],
+    imgsArray:[
+      '../../assets/images/Workshop and Meetings/UPSCAPE_Meeting_Group_photo-MAR2019.jpg'
+    ]
+  },
+  {
+    Date: '12 FEB, 2019',
+    Title:'Advisory Committee Meeting on “Urban Floods Model for Bangalore City”',
+    Details: 'The first Advisory Committee meeting on the project “Urban Flood Model for Bangalore City” was being held on 12th March, 2019, in ICWaR. The members of Advisory Committee for the project are: Sri SV Ranganath, Former Chief Secretary, GoK; Sri Manjunath Prasad, Commissioner, BBMP; Dr. RK Dave, Founder, RESPECT, Ex. Sr. Specialist (Policy and Plans), NDMA; Dr. Veena Srinivasan, Fellow convener-Centre for Environment and Development, ATREE; Prof. BS Murty, Professor, IIT Madras; Sri S Vishwanath, Water Conservation Expert, Founder and director, Biome Solutions.',
+    'links':[
+      ''],
+    imgsArray:[
+      '../../assets/images/Workshop and Meetings/UFMBC-12Feb2019.1.jpg',
+      '../../assets/images/Workshop and Meetings/UFMBC-12Feb2019.2.jpg',
+      '../../assets/images/Workshop and Meetings/UFMBC-12Feb2019.3.jpg'
+    ]
+  },
+  {
+    Date: '26 NOV, 2018',
+    Title:'Project Meet on Accompanying The adaptation of irrigated agriculture to climate CHAnge (ATCHA)',
+    Details: 'III ANR ATCHA project meeting was held in ICWaR on 26Nov-1Dec, 2018.The ATCHA project aims to accompany the adaptation of farming systems to climate change by combining an integrated biophysical model with a participatory approach in a network of experimental watersheds in the Karnataka state. The ATCHA project is based on (1) the strong partnership initiated with the International Joint Laboratory IFCWS (Indo-French Cell for Water Sciences, involving the Indian Institute of Science, Bangalore) which allowed to build an extensive database in the Berambadi experimental watershed (Critical Zone Observatory, ORE BVET) and (2) a specific Indo-French project (CEFIPRA AICHA, 2013-2016) in which an integrated model combining hydrology (AMBHAS), agronomy (STICS), economy (MoGire) and farmer decision (Namaste) models was developed.',
+    'links':[
+      ''],
+    imgsArray:[
+      '../../assets/images/Workshop and Meetings/ATCHA-III.jpg'
+    ]
+  },
+  {
+    Date: '12 SEP, 2018',
+    Title:'Project Meet on Coupled Human and Natural System Environment (CHANSE)',
+    Details: 'A project review meeting in ICWAR for the collaborative project with Imperial College London, British Geological Survey, IIT Bombay, University of Exeter UK, ATREE etc., was conducted on 12th, 13th and 14th September, 2018. The overarching aim of the research is to improve mapping and quantification of dominant interactions and feedbacks between human activities and the hydro-meteorological system of the Indo-Gangetic Plain (IGP).',
+    'links':[
+      'https://www.ceh.ac.uk/our-science/projects/india-uk-water-centre-iukwc'],
+    imgsArray:[
+      '../../assets/images/Workshop and Meetings/CHANSE-Sep2018.jpg'
+    ]
+  },
+  {
+    Date: '27 JUN, 2018',
+    Title:'International Workshop on Urban Floods',
+    Details: 'International Workshop on Urban Floods conducted in ICWaR during June 27-29, 2018. Please find the link here to the “Urban Flood Model for Bangalore city”',
+    'links':[
+      'https://sites.google.com/site/urbanfloodmodelbangalore/home'],
+    imgsArray:[
+      '../../assets/images/Workshop and Meetings/workshop-on-urban-floods-28-06-2018.jpg'
+    ]
+  },
+  {
+    Date: '04 JUN, 2018',
+    Title:'Workshop on Hydrological Outlook',
+    Details: 'Workshop on Hydrological Outlook conducted during June 04-05, 2018',
+    'links':[
+      ''],
+    imgsArray:[
+      '../../assets/images/Workshop and Meetings/HydrologicalOutlook-2018.jpg'
+    ]
+  },
+  {
+    Date: '01 MAY, 2018',
+    Title:'COSMOS soil moisture sensor installed by CEH',
+    Details: 'COSMOS soil moisture sensor installed by CEH toward collaborative research in the Kabini basin (AMBHAS). Live feed (updated daily) of the COSMOS data.',
+    'links':[
+      'http://ambhas.com/'],
+    imgsArray:[
+      '../../assets/images/Workshop and Meetings/cosmos.png',
+      '../../assets/images/Workshop and Meetings/cosmos1.png'
+    ]
+  },
+  {
+    Date: '30 MAR, 2016',
+    Title:'Indo-UK Workshop on “Water Quality – Source Protection”',
+    Details: 'Indo-UK Workshop on “Water Quality – Source Protection” conducted during 30 March – 1 April, 2016. The workshop is intended to develop ideas for future scientific and  technological collaborations between the UK and India which will address key knowledge gaps in our understanding of water resources management and source protection.',
+    'links':[
+      ''],
+    imgsArray:[
+      '../../assets/images/Workshop and Meetings/Indo-UK_WQSP2016.jpg'
+    ]
+  },
+  {
+    Date: '17 NOV, 2015',
+    Title:'An Indo-French workshop on “Remote Sensing for Water and Agriculture: Current and Future Possibilities”',
+    Details: 'An Indo-French workshop on “Remote Sensing for Water and Agriculture: Current and Future Possibilities” conducted on November 17, 2015',
+    'links':[
+      ''],
+    imgsArray:[
+      '../../assets/images/Workshop and Meetings/if_workshop.jpg'
+    ]
+  },
+  {
+    Date: '13 APR, 2015',
+    Title:'An Indo-UK workshop on “Water Security”',
+    Details: 'An Indo-UK workshop on “Water Security” conducted during April 13-14, 2015',
+    'links':[
+      ''],
+    imgsArray:[
+      '../../assets/images/Workshop and Meetings/WaterSecurity-2015.jpg'
+    ]
+  },
 ]
