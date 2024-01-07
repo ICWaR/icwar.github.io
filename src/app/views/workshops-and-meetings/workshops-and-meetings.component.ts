8b3dd500b3ef30869444de8abbff87ec21d18fc8
architbubber <architbubber07@gmail.com>
2024-01-08 03:00:41 +0530

    Temporary Fix to make workshops and Seminars page working

--- src/app/views/workshops-and-meetings/workshops-and-meetings.component.ts
+++ src/app/views/workshops-and-meetings/workshops-and-meetings.component.ts
@@ -1,13 +1,43 @@
-import { Component } from '@angular/core';
+import { seminars } from 'src/data-entries/json/Seminars';
+import { Component} from '@angular/core';
 import { workshops } from 'src/data-entries/json/workshops';
+
 @Component({
   selector: 'app-workshops-and-meetings',
   templateUrl: './workshops-and-meetings.component.html',
   styleUrls: ['./workshops-and-meetings.component.scss']
 })
+
 export class WorkshopsAndMeetingsComponent {
-workshops = workshops;
-removeSpaceFromString(str:string){
-  return str.replaceAll(' ','_')
-}
+  $=$;
+  workshops = workshops;
+  seminars = seminars;
+
+  ngOnInit(): void {
+
+
+  }
+
+   updateData(dataType:string){
+      if(dataType === 'Workshops' ||  dataType === 'Meetings'){
+        $('#Workshops').removeClass('d-none')
+      $('#Seminars').addClass('d-none')
+    }
+    else{
+      $('#Workshops').addClass('d-none')
+      $('#Seminars').removeClass('d-none')
+    }
+
+    $('ol>button').each(function () {
+      if(this.innerHTML !== dataType)
+        $(this).removeClass('font-highlight')
+      else
+        $(this).addClass('font-highlight')
+    });
+
+  }
+
+  removeSpaceFromString(str:string){
+    return str.replaceAll(' ','_')
+  }
 }
