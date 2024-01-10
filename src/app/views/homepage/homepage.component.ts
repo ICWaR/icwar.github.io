cbe2d90712a0dba54d781191eeaacca2c755a2b6
architbubber <architbubber07@gmail.com>
2024-01-10 17:38:46 +0530

    Direct link opening now supported with redirect from 404 page

--- src/app/views/homepage/homepage.component.ts
+++ src/app/views/homepage/homepage.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {homepageData} from '../../../data-entries/json/homepage';
 import { newsJson } from 'src/data-entries/json/news';
+import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
@@ -12,9 +13,14 @@ export class HomepageComponent {
   data =  homepageData;
   slides=['https://www.gfz-potsdam.de/fileadmin/_processed_/8/8/csm_grace_sticker_01_94f4685989.jpeg','../../../assets/images/Slide/slideshow3.jpg','https://img.rasset.ie/0010713d-500.jpg']
   news=newsJson;
-
+  constructor(
+    private route: ActivatedRoute,private router: Router
+  ) {}
   ngOnInit(): void {
-
+    let redirectTo = this.route.snapshot.queryParamMap.get('route');
+    if(redirectTo!=null){
+      this.router.navigate([redirectTo])
+    }
   }
   ngAfterViewInit(): void {
     //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
