0d5f67d347227820a2edd5472040fcba369d9a9e
krispy <krishnapandeykp@users.noreply.github.com>
2024-03-14 10:46:39 +0530

    Changes reverted for Monsoon page

--- src/app/views/activities-and-events/activities-and-events.component.ts
+++ src/app/views/activities-and-events/activities-and-events.component.ts
@@ -1,28 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { monsoonPageData } from '../../../data-entries/json/monsoon-schools';
-
-// Define a union type for the allowed values of selectedYear
-type YearOption = "2023" | "2019" | "2018" | "2017";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-activities-and-events',
   templateUrl: './activities-and-events.component.html',
   styleUrls: ['./activities-and-events.component.scss']
 })
-export class ActivitiesAndEventsComponent implements OnInit {
-  selectedYear: YearOption = '2023'; // Set default year to 2023
-  eventData: any;
+export class ActivitiesAndEventsComponent {
 
-  constructor() {}
-
-  ngOnInit(): void {
-    this.selectYear(this.selectedYear);
-  }
-
-  selectYear(year: keyof typeof monsoonPageData): void {
-    this.selectedYear = year;
-    this.eventData = monsoonPageData[year];
-  }
 }
-
 
