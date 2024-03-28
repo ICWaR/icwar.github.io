83204468f6454511a37c5ddca8bc9b48b397e91a
krispy <krishnapandeykp@users.noreply.github.com>
2024-03-28 17:03:24 +0530

    Changes Done for Sidebar, and Fallback functionality of Seminars for Workshop Component has been improved.
    Earlier if we opened the seminar and click on link to join, and if we switch back, it used to fall on workshops but now it remains on seminar page only.  But direct access to seminars section from navbar is yet to be achieved.

--- src/app/views/workshops-and-meetings/workshops-and-meetings.component.ts
+++ src/app/views/workshops-and-meetings/workshops-and-meetings.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { workshops } from 'src/data-entries/json/workshops';
 import { seminars } from 'src/data-entries/json/Seminars';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
@@ -10,7 +10,7 @@ import { SecurityContext } from '@angular/core';
   templateUrl: './workshops-and-meetings.component.html',
   styleUrls: ['./workshops-and-meetings.component.scss']
 })
-export class WorkshopsAndMeetingsComponent {
+export class WorkshopsAndMeetingsComponent implements OnInit {
   workshops = workshops;
   seminars = seminars;
   showWorkshops: boolean = true;
@@ -20,13 +20,20 @@ export class WorkshopsAndMeetingsComponent {
     private sanitizer: DomSanitizer,
     private router: Router,
     private route: ActivatedRoute
-  ) {
-    // Get the current route URL and set the visibility accordingly
-    const currentRoute = this.router.url.split('/')[1];
-    if (currentRoute === 'Seminars') {
-      this.showWorkshops = false;
-      this.showSeminars = true;
-    }
+  ) {}
+
+  ngOnInit(): void {
+    // Subscribe to route parameter changes
+    this.route.queryParams.subscribe(params => {
+      const type = params['type'];
+      if (type === 'Seminars') {
+        this.showWorkshops = false;
+        this.showSeminars = true;
+      } else {
+        this.showWorkshops = true;
+        this.showSeminars = false;
+      }
+    });
   }
 
   // Sanitize the HTML content before binding it
@@ -40,16 +47,5 @@ export class WorkshopsAndMeetingsComponent {
   // Function to replace spaces in string with underscores
   removeSpaceFromString(str: string): string {
     return str.replace(/ /g, '_');
-  }
-
-  // Update the visibility based on the selected option
-  updateData(dataType: string) {
-    if (dataType === 'Workshops') {
-      this.showWorkshops = true;
-      this.showSeminars = false;
-    } else {
-      this.showWorkshops = false;
-      this.showSeminars = true;
-    }
   }
 }
